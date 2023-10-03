cfec048c5de47a5739c8211e44aa19a54c76d29e
leramsey <lramsey@codingdojo.com>
2023-10-03 09:24:00 -0700

    Code cleanup and comments

--- popup.js
+++ popup.js
@@ -1,34 +1,33 @@
-async function getCurrentTab() {
+async function getCurrentTab() { // gets active tab info and returns object
   let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let tab = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
+// post active tab info when Save clicked. 
+// Will change after authentication is implemented
 document.getElementById('savebutton').addEventListener('click', async () => {
-      const tabInfo = await getCurrentTab()
-    
-      const username = document.getElementById('username-input').value;
-      console.log(tabInfo[0].title)
-      const newTab = {
-        username: username,
-        title: tabInfo[0].title,
-        url: 'hello'
-      }
-      console.log(newTab)
-      fetch('http://127.0.0.1:3000/api/addtab', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(newTab)
-      })
-        .then(res => console.log(JSON.stringify(res.json())))
-    })
+  const tabInfo = await getCurrentTab()
+  const username = document.getElementById('username-input').value;
+  console.log(tabInfo[0].title)
+  const newTab = {
+    username: username,
+    title: tabInfo[0].title,
+    url: 'hello'
+  }
+  fetch('http://127.0.0.1:3000/api/addtab', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newTab)
+  })
+    .then(res => console.log(JSON.stringify(res.json()))) // remove when add element is complete
+})
 
-
+// gets all tabs for username raw json for now for testing purposes only
 document.getElementById('getbutton').addEventListener('click', () => {
   const username = document.getElementById('username-input').value;
   if (username.length < 1) {
-    alert('You must enter a username')
+    alert('You must enter a username') // Will be better error message later i.e. toast
   }
   else {
     fetch(`http://127.0.0.1:3000/api/get-tabs/${username}`)
@@ -39,9 +38,3 @@ document.getElementById('getbutton').addEventListener('click', () => {
       })
   }
 })
-
-// function saveTabinfo() {
-
-// }
-
-
